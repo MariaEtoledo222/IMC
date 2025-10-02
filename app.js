f1ede52b0d5ba89fdc9b935de83ef4264502296b
Maria Eduarda Toledo Reis <mariareistoledo17@gmail.com>
2025-10-02 17:22:05 -0300

    criado lógica de avaliação do IMC do Paciente.

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 let nomedoPaciente = "Marcos",
 alturaPaciente = 1.95,
-pesoPaciente = 90,
+pesoPaciente = 140,
 resultado = pesoPaciente / (alturaPaciente*alturaPaciente);
 
-console.log("O paciente " + nomedoPaciente + " possui um IMC de: " + resultado.toFixed(1))
+// console.log("O paciente " + nomedoPaciente + " possui um IMC de: " + resultado.toFixed(1))
+
+if(resultado < 18.5){
+    console.log("O paciente " + nomedoPaciente + " possui um IMC de: " + resultado.toFixed(1) + " e esta com baixo peso ")
+} else if (resultado >= 18.5 && resultado <= 24.9){
+    console.log("O paciente " + nomedoPaciente + " possui um IMC de: " + resultado.toFixed(1) + " e esta com o peso normal ")
+}else{
+    console.log("O paciente " + nomedoPaciente + " possui um IMC de: " + resultado.toFixed(1) + " e esta com sobre peso ")
+}
