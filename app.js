5f2d5c4c68594f02c8f8f40dac4f0ea6620e8e45
Maria Eduarda Toledo Reis <mariareistoledo17@gmail.com>
2025-10-02 16:53:47 -0300

    Envio inicial

---
+++ app.js
@@ -0,0 +1,6 @@
+let nomedoPaciente = "Marcos",
+alturaPaciente = 1.95,
+pesoPaciente = 90,
+resultado = pesoPaciente / (alturaPaciente*alturaPaciente);
+
+console.log("O paciente " + nomedoPaciente + " possui um IMC de: " + resultado.toFixed(1))
